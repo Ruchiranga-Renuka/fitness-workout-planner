390f9805bcf97ec32808470400d2187f628a24ff
Ruchiranga <mr.ruchirangarenuka@gmail.com>
2026-09-08 18:30:19 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,15 +22,47 @@ class FitnessApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const seedColor = Color(0xFF166534);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Fitness Workout Planner',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.green,
+          seedColor: seedColor,
+          brightness: Brightness.light,
         ),
         textTheme: GoogleFonts.poppinsTextTheme(),
         useMaterial3: true,
+        scaffoldBackgroundColor: const Color(0xFFF7F9F6),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          scrolledUnderElevation: 0,
+          centerTitle: false,
+        ),
+        cardTheme: CardThemeData(
+          elevation: 0,
+          margin: EdgeInsets.zero,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(18)),
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(14)),
+            borderSide: BorderSide.none,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(14)),
+            borderSide: BorderSide(color: Color(0xFFE2E8E2)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(14)),
+            borderSide: BorderSide(color: seedColor, width: 2),
+          ),
+        ),
       ),
       home: const HomeScreen(),
     );
